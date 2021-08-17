360462255a18f947c220f42c213911a165eac434
lisahuele <lisahuele@gmail.com>
2021-08-17 18:44:51 -0400

    began prompts

--- server.js
+++ server.js
@@ -19,30 +19,27 @@ db.connect(err => {
 });
 
 function promptUser() {
-
-    function directory() {
-        inquirer.prompt([
-            {
-                type: "list",
-                name: "choice",
-                message: "What would you like to do?",
-                choices: [
-                    "View All Departments",
-                    "View All Roles",
-                    "View All Employees",
-                    "Add a Department",
-                    "Add a Role",
-                    "Add an Employee",
-                    "Update an Employee"
-                ]
-            }
-        ])
-        .then(function(input) {
-            switch(input.choice) {
-                case "View All Departments":
-                    viewAllDepartments();
-                break;
-            }
-        })
-    }
+    inquirer.prompt([
+        {
+            type: "list",
+            name: "choice",
+            message: "What would you like to do?",
+            choices: [
+                "View All Departments",
+                "View All Roles",
+                "View All Employees",
+                "Add a Department",
+                "Add a Role",
+                "Add an Employee",
+                "Update an Employee"
+            ]
+        }
+    ])
+    .then(function(input) {
+        switch(input.choice) {
+            case "View All Departments":
+                viewAllDepartments().then(promptUser());
+            break;
+        }
+    })
 }
